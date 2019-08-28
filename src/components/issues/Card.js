06529675221de7f6147143945569025ba2213f1f
saravind23399 <aravind@inno8lab.in>
2019-08-28 22:40:09 +0530

    Changes

--- src/components/issues/Card.js
+++ src/components/issues/Card.js
@@ -33,17 +33,22 @@ function IssueCard(props) {
 	return (
 		<div className="card pr-5 pl-5 pb-3 shadow">
 			<div className="card-body">
-				<h4
-					className="card-title"
-					role="button"
-					data-toggle="collapse"
-					data-target={`#collapseDescription${props.issueId}`}>
-					<span className={iconAndColor.color}>
+				<h4 className="card-title">
+					<span
+						className={`${iconAndColor.color} w-100`}
+						role="button"
+						data-toggle="collapse"
+						data-target={`#collapseDescription${props.issueId}`}>
 						<i className={iconAndColor.icon + ' mr-4'} />
 						{props.title}
 					</span>
 					<i
-						className="fa text-danger fa-trash float-right clickable-icon"
+						className="fa text-info fa-chevron-down float-right clickable-icon"
+						role="button"
+						data-toggle="collapse"
+						data-target={`#collapseDescription${props.issueId}`}></i>
+					<i
+						className="fa text-danger fa-trash float-right clickable-icon mr-5"
 						onClick={() => {
 							props.handleDeleteIssue(props.issueId)
 						}}
@@ -58,7 +63,7 @@ function IssueCard(props) {
 					/>
 				</h4>
 				<div
-					class="collapse m-5"
+					class="collapse m-3 mt-4"
 					id={`collapseDescription${props.issueId}`}>
 					<span className="card-subtitle mb-2 text-muted">
 						<h5>Description</h5>
